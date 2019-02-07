fa385732f62313014dc2474b8a43ba867ad11c81
Thorsten <thorsten.meister@bbv.ch>
2019-02-07 10:57:51 +0100

    initial commit

---
+++ pages/about.js
@@ -0,0 +1,9 @@
+import Header from '../components/Header';
+
+
+export default () => (
+    <div>
+        <Header />
+        <p>This is the about page</p>
+    </div>
+)
---
+++ pages/index.js
@@ -0,0 +1,9 @@
+import Header from '../components/Header';
+
+
+export default () => (
+    <div>
+        <Header />
+        <p>Hello Next.js</p>
+    </div>
+)
